193f22c2fb9102c80b0e2b8b3268ec8e2f1968a0
Kaylie Chang <firenachang@gmail.com>
2026-06-29 13:39:59 +0900

    feat: can view rows of car stats

--- client/src/pages/ViewCars.jsx
+++ client/src/pages/ViewCars.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import '../App.css'
+import { useState, useEffect } from 'react'
 
 const ViewCars = () => {
+    const [cars, setCars] = useState([])
     
+    useEffect(() => {
+        const fetchCars = async () => {
+            try {
+                const response = await fetch('http://localhost:3000/car')
+
+                if (!response.ok) {
+                    throw new Error('Failed to fetch all cars')
+                }
+
+                const data = await response.json()
+                setCars(data)
+            } catch (err) {
+                console.log("Error in getting cars");
+            }
+        }
+
+        fetchCars()
+    }, [])
+
     return (
         <div>
-            
+            {cars.map((car) => (
+                <div key={car.id}>
+                    <h1>{car.exterior_id}</h1>
+                    <h1>{car.interior_id}</h1>
+                    <h1>{car.wheels_id}</h1>
+                    <h1>{car.roof_id}</h1>
+                </div>
+            ))}
         </div>
     )
 }
